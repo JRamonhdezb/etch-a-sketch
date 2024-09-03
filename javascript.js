a39602f5dcf46da1e393e2b99bf85c9e442b3e25
JRamonhdezb <141176160+JRamonhdezb@users.noreply.github.com>
2024-09-03 14:39:12 -0700

    Add new grid-square button and buttonEventListener
    The new button will ask to the user the size of the grid square. When
    click event is listened it will prompt window that ask a number that
    control the grid square size, then remove all div nested inside the
    container and calls the function getGridSquare.

--- javascript.js
+++ javascript.js
@@ -1,7 +1,11 @@
 console.log("Hello World");
 
-
+const body = document.querySelector("body");
 const container = document.querySelector("#container");
+
+const button = document.createElement("button");
+button.textContent = "Start a new grid square";
+body.insertBefore(button, container);
 
 const getGridSquare = function(n) {
     for (let i = 0; i < n; i++){
@@ -23,6 +27,22 @@ const addEventListener = function(element) {
     })
 }
 
-let number = 16; 
-getGridSquare(number);
+const clearFunction = function() {
+    while(container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+} 
 
+// let number = 16; 
+// getGridSquare(number);
+
+button.addEventListener("click", () => {
+    let number;
+    do {
+        number = +prompt("New grid square size: ", 16);
+        if (number > 100) alert ("MAX number is 100. Try again.")
+    } while (number > 100);
+    
+    clearFunction(); 
+    getGridSquare(number);
+})
