eb758d63941a78963b15a3fdae5ebd368ec1de46
JRamonhdezb <141176160+JRamonhdezb@users.noreply.github.com>
2024-09-03 11:34:58 -0700

    Add getGridSquare function
    The function will return a grid square, this function works with two
     for nested loops that create and add to the DOM the div nodes that
     form the columns and rows in the container. The function takes a number
     argument which defines the n x n grid

---
+++ javascript.js
@@ -0,0 +1,21 @@
+console.log("Hello World");
+
+
+const container = document.querySelector("#container");
+
+const getGridSquare = function(n) {
+    for (let i = 0; i < n; i++){
+        let column = document.createElement("div");
+        column.classList.add("column");
+        for (let i = 0; i < n; i++) {
+            let cell = document.createElement("div");
+            cell.classList.add("cell");
+            column.appendChild(cell);
+        }
+        container.appendChild(column);
+    }
+}
+
+let number = 16; 
+getGridSquare(number);
+
