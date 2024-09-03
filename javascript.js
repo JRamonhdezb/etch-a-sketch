2d877c619522a03216415886ca23bc33f43104cc
JRamonhdezb <141176160+JRamonhdezb@users.noreply.github.com>
2024-09-03 12:46:17 -0700

    Add event listener to each cell in grid square
    Each cell was assigned a mouse over event listener which assigns a new
     class to the cell, the css class just sets a black background color to
     the div. Then the function is called in the for loop that creates each
     cell.

--- javascript.js
+++ javascript.js
@@ -10,10 +10,17 @@ const getGridSquare = function(n) {
         for (let i = 0; i < n; i++) {
             let cell = document.createElement("div");
             cell.classList.add("cell");
+            addEventListener(cell);
             column.appendChild(cell);
         }
         container.appendChild(column);
     }
+}
+
+const addEventListener = function(element) {
+    element.addEventListener("mouseover", () => {
+        element.classList.add("cellHover")
+    })
 }
 
 let number = 16; 
